93e18fafccd5bc5a8845f6ed6c551740004adae6
97kristr <kristoofer9@hotmail.com>
2015-12-01 09:27:54 +0100

    Initial commit
    skeleton for the wep project.

---
+++ flask_app.py
@@ -0,0 +1,24 @@
+"""Author: krille, 2015"""
+
+
+from flask import Flask,render_template
+#importerar flask och render_template
+
+app = Flask(__name__)
+#starta flask_app
+
+@app.route('/')
+#@ före en funktion är en creator
+@app.route('/index/')
+#en hemsida kan ha flera sidor
+
+def startpage():
+    #funktionen som startar sidan.
+    return render_template("index.html")
+    #skickar tillbaka en "return" till klienten
+    
+
+if (__name__ == "__main__"):
+    #name=filen vi är i, main=filen vi executerar.
+    app.run()
+    #kör appen
